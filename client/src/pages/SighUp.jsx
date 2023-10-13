767840eb2f3ed3bf48f4f14ed657e7de16a7b1fa
brightnkhoma <143875131+brightnkhoma@users.noreply.github.com>
2023-10-13 20:20:23 +0100

    complete sign up page functionality

--- client/src/pages/SighUp.jsx
+++ client/src/pages/SighUp.jsx
@@ -1,14 +1,52 @@
-import {Link} from 'react-router-dom'
+import { useState,  } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function SighUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] =useState(false);
+  const navigate= useNavigate();
+  const handleChange= (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+    
+  }
+  const handleSubmit= async (e)=>{
+    try{
+      
+      e.preventDefault();
+      setLoading(true);
+      setError(null)
+      
+      const res= await fetch('/api/auth/signup',{
+        method : 'POST',
+        headers: {
+          'content-type' : 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+      
+      const data= await res.json();     
+      setLoading(false);
+     console.log(data);
+     navigate('/sign-in');
+    }
+    catch(e){
+      setLoading(false);
+      setError(e.message);
+    }
+  }
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
        <h1 className='text-3xl text-center font-semibold my-7'>Sign up</h1>
-       <form  className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='p-3 rounded-lg' id='email'/>
-        <input type="password" placeholder='password' className='p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 p-3 rounded-lg uppercase text-white hover:opacity-90 disabled:opacity-80'>sign up</button>
+       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type="email" placeholder='email' className='p-3 rounded-lg' id='email'onChange={handleChange}/>
+        <input type="password" placeholder='password' className='p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='bg-slate-700 p-3 rounded-lg uppercase text-white hover:opacity-90 disabled:opacity-80'>{loading ? 'loading...' : 'sign up'}</button>
        </form>
        
          <div className='flex gap-2 mt-5'>
@@ -17,9 +55,10 @@ export default function SighUp() {
           <span className='text-blue-700'>sign in</span>
         </Link>
         
-       </div>
-       
-      
+       </div>  
+       <div>
+        {error && <p className='text-red-500 mt-5'>{error}</p>}        
+        </div>       
 
     </div>
    
