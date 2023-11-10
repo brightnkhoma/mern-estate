21b9860aa29f0e29bd5640b2013e63b00ef8ba2f
brightnkhoma <gis-020-20@must.ac.mw>
2023-11-10 18:42:48 +0100

    update the header and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SighUp from "./pages/SighUp"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
@@ -13,8 +14,10 @@ export default function App() {
     <Routes>
       <Route path={'/'} element={<Home/>}/>
       <Route path={'sign-in'} element={<SignIn/>}/>
-      <Route path={'sign-up'} element={<SighUp/>}/>
-      <Route path={'profile'} element={<Profile/>}/>
+      <Route path={'sign-up'} element={<SighUp/>}/>      
+      <Route element={<PrivateRoute/>}>
+        <Route path={'profile'} element={<Profile/>}/>
+      </Route>
       <Route path={'about'} element={<About/>}/>
     </Routes>
     </BrowserRouter>
